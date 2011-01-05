e5104f3c94407d7adbcd6faa385c5d8dbe89ec0f
Eric Wilson <ewilson@pillartechnology.com>
2011-01-05 12:37:12 -0500

    Added triples, need to find correct contenders

--- 012.hs
+++ 012.hs
@@ -1,23 +1,31 @@
-divisors :: Int -> [Int]
+divisors :: Integer -> [Integer]
 divisors n = [ d | d <- [1..n], mod n d == 0 ]
 
-numDivs :: Int -> Int
+numDivs :: Integer -> Int
 numDivs = length . divisors
 
-tri :: Int -> Int
+tri :: Integer -> Integer
 tri n = n * (n+1) `quot` 2
 
-hm :: Int -> Int
+hm :: Integer -> Int
 hm n = if (even n)
 			then numDivs (quot n 2) * numDivs (n+1)
 			else numDivs n * numDivs (quot (n+1) 2)
 
-contenders :: Int -> [Int]
+contenders :: Integer -> [Integer]
 contenders n = twosMap [1..n]
 
-twosMap :: [Int] -> [Int]
+twosMap :: [Integer] -> [Integer]
 twosMap [] = []
 twosMap (x:xs) = twos x ++ twosMap xs
 		  
-twos :: Int -> [Int]
+twos :: Integer -> [Integer]
 twos n = (2^n - 1) : [2^n]
+
+triDivTriple :: Integer -> (Integer, Integer, Int)
+triDivTriple n = (n, tri n, hm n)
+
+tripleAbove :: Int -> (Integer, Integer, Int) -> Bool
+tripleAbove n (_, _, m) = (m > n)
+
+{-- filter (tripleAbove 500) $ map triDivTriple $ contenders 20 --}
