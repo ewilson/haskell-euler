bb2c62381cb627f44b5284523850aa78d00d1c40
Eric Wilson <ewilson@pillartechnology.com>
2011-01-05 21:32:57 -0500

    refactord 002

--- 002.hs
+++ 002.hs
@@ -1,9 +1,4 @@
-fib :: Int -> Int
-fib n = fst $ fibPair n
-	where 
-		fibPair 1 = (1,1)
-		fibPair n = fibNext $ fibPair (n-1)
-		fibNext (x,y) = (y, x+y)
+import SimpleMath
 
 euler2 :: Int -> Int
 euler2 n = sum $ filter even $ takeWhile (< n) [ fib k | k <- [1..] ]
--- SimpleMath.hs
+++ SimpleMath.hs
@@ -1,7 +1,15 @@
 module SimpleMath  
-( divisors  
+( fib
+, divisors  
 , numDivs
 ) where 
+
+fib :: Int -> Int
+fib n = fst $ fibPair n
+	where 
+		fibPair 1 = (1,1)
+		fibPair n = fibNext $ fibPair (n-1)
+		fibNext (x,y) = (y, x+y)
 
 divisors :: Integer -> [Integer]
 divisors n = [ d | d <- [1..n], mod n d == 0 ]
