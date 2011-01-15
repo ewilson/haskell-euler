81e0661649d7c603cb1f7cbb26ec1586416851d8
Eric Wilson <ewilson@pillartechnology.com>
2011-01-15 08:57:47 -0500

    added max function

--- 011.hs
+++ 011.hs
@@ -3,3 +3,7 @@ stringToIntArray = stringArrayToIntArray . stringToArray
 	where 
 		stringToArray = map words . lines
 		stringArrayToIntArray = map $ map read
+		
+findMax :: [Int] -> Int
+findMax = head . foldl next [0,0,0,0]
+	where next (oldMax: old: olds) new = max oldMax (old * new * product olds) : olds ++ [new] 
