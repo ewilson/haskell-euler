ad8582b4c4348fab735e863bbd338f67176013dd
Eric Wilson <ewilson@pillartechnology.com>
2011-01-17 12:43:30 -0500

    completed function for 13, need io

--- 013.hs
+++ 013.hs
@@ -11,3 +11,12 @@ carry :: [Int] -> [Int]
 carry (n:m:ms) = mod n 10 : carry ((m + quot n 10) : ms)
 carry (0:[]) = []
 carry (n:[]) = carry [n,0]
+
+input :: String -> [[Int]]
+input = map toBigInt . lines
+
+toString :: [Int] -> [Char]
+toString = map intToDigit . reverse
+
+euler13 :: String -> String
+euler13 = take 10 . toString . bigSum . input
