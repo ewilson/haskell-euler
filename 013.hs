6109dacb5e1a8e72b0e59050c5f6c7150460f0f6
Eric Wilson <ewilson@pillartechnology.com>
2011-01-22 15:29:22 -0500

    Refactored for readability

--- 013.hs
+++ 013.hs
@@ -1,19 +1,16 @@
 import Data.List
 import Data.Char
 
-toBigInt :: [Char] -> [Int]
-toBigInt = reverse . map digitToInt
-
 bigSum :: [[Int]] -> [Int]
 bigSum = carry . map sum . transpose
-
-carry :: [Int] -> [Int]
-carry (n:m:ms) = mod n 10 : carry ((m + quot n 10) : ms)
-carry (0:[]) = []
-carry (n:[]) = carry [n,0]
+	where
+		carry (n:m:ms) = mod n 10 : carry ((m + quot n 10) : ms)
+		carry (0:[]) = []
+		carry (n:[]) = carry [n,0]
 
 input :: String -> [[Int]]
 input = map toBigInt . lines
+	where toBigInt = reverse . map digitToInt
 
 toString :: [Int] -> [Char]
 toString = map intToDigit . reverse
