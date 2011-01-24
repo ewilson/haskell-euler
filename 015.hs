ebecc2b953ca9a9faf40da023e4d7fb2da015609
Eric Wilson <ewilson@pillartechnology.com>
2011-01-24 11:03:48 -0500

    Refactored 15

--- 015.hs
+++ 015.hs
@@ -1,10 +1,9 @@
-selfCompose :: (a -> a) -> Int -> a -> a
-selfCompose f n = foldl (.) id fs
-	where fs = replicate n f
-	
-nthRow :: (Integral a) => Int -> [a]
-nthRow n = selfCompose nextRow n $ [1]
+pascal :: (Integral a) => Int -> [[a]]
+pascal n = take (n+1) $ iterate nextRow [1]
 	where nextRow xs = zipWith (+) (0:xs) (xs ++ [0])
 
+binom :: (Integral a) => Int -> Int -> a
+binom n k = last (pascal n) !! k
+	
 euler15 :: (Integral a) => Int -> Int -> a
-euler15 rows columns = nthRow (rows + columns) !! rows
+euler15 rows columns = binom (rows + columns) columns
