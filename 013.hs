818b01680cf0ac551c18d64a78bbb4789a61b58a
Eric Wilson <ewilson@pillartechnology.com>
2011-01-25 12:27:20 -0500

    Refactored, using type synonym for clarity

--- 013.hs
+++ 013.hs
@@ -2,18 +2,20 @@ import Data.List
 import Data.Char
 import EulerUtil.Io
 
-bigSum :: [[Int]] -> [Int]
+type BigNat = [Int]
+
+bigSum :: [BigNat] -> BigNat
 bigSum = carry . map sum . transpose
 	where
 		carry (n:m:ms) = mod n 10 : carry ((m + quot n 10) : ms)
 		carry (0:[]) = []
 		carry (n:[]) = carry [n,0]
 
-input :: String -> [[Int]]
-input = map toBigInt . lines
-	where toBigInt = reverse . map digitToInt
+input :: String -> [BigNat]
+input = map toBigNat . lines
+	where toBigNat = reverse . map digitToInt
 
-toString :: [Int] -> [Char]
+toString :: BigNat -> [Char]
 toString = map intToDigit . reverse
 
 euler13 :: String -> String
