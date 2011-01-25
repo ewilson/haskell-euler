66a65ac47fe2a3ed51983dbc0b6c82f1f1ac309c
Eric Wilson <ewilson@pillartechnology.com>
2011-01-25 06:33:11 -0500

    more experimentation with 14

--- 014.hs
+++ 014.hs
@@ -1,9 +1,27 @@
-loop :: Int -> [Int]
+import qualified Data.Map as Map  
+import Data.Maybe
+
+loop :: (Integral a) => a -> [a]
 loop 1 = [1]
 loop n = n : (loop $ next n)
-	where next n = 
-		if mod n 2 == 0
-			then quot n 2
-			else 3 * n + 1
+
+
+next :: (Integral a) => a -> a
+next n = 
+	if mod n 2 == 0
+		then quot n 2
+		else 3 * n + 1
 -- answer must be > N/2
 -- answer must not be congruent to 2, 4, 5 mod 6
+-- answer must not be congruent to 1 mod 6 unless > 750000
+
+testMap = 
+	[(1,1)
+	,(2,2)
+	,(3,8)
+	,(4,3)
+	,(5,6)
+	]
+	
+newLoop :: (Integral a) => a -> [a]
+newLoop n = takeWhile (>=n) $ loop n
