6b38dfc77587f8bc0e0660d7823fbe6f62188b79
Eric Wilson <ewilson@pillartechnology.com>
2011-01-29 16:40:35 -0500

    more date functions

--- 019.hs
+++ 019.hs
@@ -29,8 +29,25 @@ daysInMonth date
 			m = month date
 			y = year date
 			
-dayShiftFromYearChange :: Year -> Year -> Int
-dayShiftFromYearChange start end = mod7 (sum $ map daysInYear [start..end-1])
+epoch :: Date
+epoch = Date 1900 1 1
+			
+daysInPreviousYearsSinceEpoch :: Year -> Int
+daysInPreviousYearsSinceEpoch currentYear = sum $ map daysInYear [year epoch..currentYear-1]
 
+daysInPreviousMonthsInCurrentYear :: Date -> Int
+daysInPreviousMonthsInCurrentYear date = sum $ map daysInMonth (previousMonths date)
+
+previousMonths :: Date -> [Date]
+previousMonths date = [ Date (year date) m 1 | m <- [1..(month date)-1]]
+
+daysSinceEpoch :: Date -> Int
+daysSinceEpoch date = daysInPreviousYearsSinceEpoch (year date)
+					  + daysInPreviousMonthsInCurrentYear date
+					  + (day date - 1)
+
+monthsInYears :: Year -> Year -> [Date]
+monthsInYears start end = [ Date y m 1 | y <- [start..end], m<-[1..12] ]
+					  
 mod7 :: Int -> Int
 mod7 n = mod n 7
