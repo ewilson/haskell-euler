3308e26e905c19300d9818788147aa0c7f6a4b49
Eric Wilson <ewilson@pillartechnology.com>
2011-02-03 12:32:24 -0500

    more work on 17

--- 017.hs
+++ 017.hs
@@ -1,16 +1,28 @@
-import qualified Data.Map as Map
-import Data.Maybe
-
-digits = [0..9]
-
 onesWords = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 tensWords = ["", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+teenWords = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
 
-onesMap = Map.fromList $ zip digits onesWords
-tensMap = Map.fromList $ zip digits tensWords
+digitsToWords :: (Int, Int, Int) -> (String, String, String)
+digitsToWords (h, t, o) = (onesWords !! h, tensWords !! t, onesWords !! o)
 
-onesToWord :: Int -> String
-onesToWord n = fromJust $ Map.lookup n onesMap
+doubleToWords :: (Int, Int) -> String
+doubleToWords (t, o) 
+	| t == 1    = teenWords !! o
+	| otherwise = hyphenate (tensWords !! t, onesWords !! o)
+	where
+		hyphenate ("", oWord) = oWord 
+		hyphenate (tWord, "") = tWord 
+		hyphenate (tWord, oWord) = tWord ++ "-" ++ oWord 
 
-tensToWord :: Int -> String
-tensToWord n = fromJust $ Map.lookup n tensMap
+tripleToWords :: (Int, (Int, Int)) -> String
+tripleToWords (h, to) = hundredAnd (onesWords !! h, doubleToWords to)
+	where
+		hundredAnd ("", toWord) = toWord
+		hundredAnd (hWord, "") = hWord ++ " hundred"
+		hundredAnd (hWord, toWord) = hWord ++ " hundred and " ++ toWord
+		
+makeTriple :: Int -> (Int, (Int, Int))
+makeTriple n = (quot n 100, (mod (quot n 10) 10, mod n 10))
+
+threeDigitsToWords :: Int -> String
+threeDigitsToWords = tripleToWords . makeTriple
