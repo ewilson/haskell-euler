e7d5bdc41eda2b4348307c40a24a164d157739f8
Eric Wilson <ewilson@pillartechnology.com>
2011-02-04 09:06:28 -0500

    more on 17

--- 017.hs
+++ 017.hs
@@ -4,24 +4,25 @@ onesWords = ["", "one", "two", "three", "four", "five", "six", "seven", "eight",
 tensWords = ["", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
 teenWords = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
 
+tensConnect = ("", "-")
+hundredsConnect = (" hundred", " and ")
+thousandsConnect = (" thousand", " , ")
+
 digitsToWords :: (Int, Int, Int) -> (String, String, String)
 digitsToWords (h, t, o) = (onesWords !! h, tensWords !! t, onesWords !! o)
 
 doubleToWords :: (Int, Int) -> String
 doubleToWords (t, o) 
 	| t == 1    = teenWords !! o
-	| otherwise = hyphenate (tensWords !! t, onesWords !! o)
-	where
-		hyphenate ("", oWord) = oWord 
-		hyphenate (tWord, "") = tWord 
-		hyphenate (tWord, oWord) = tWord ++ "-" ++ oWord 
+	| otherwise = connect (tensWords !! t) (onesWords !! o) tensConnect
 
 tripleToWords :: (Int, (Int, Int)) -> String
-tripleToWords (h, to) = hundredAnd (onesWords !! h, doubleToWords to)
-	where
-		hundredAnd ("", toWord) = toWord
-		hundredAnd (hWord, "") = hWord ++ " hundred"
-		hundredAnd (hWord, toWord) = hWord ++ " hundred and " ++ toWord
+tripleToWords (h, to) = connect (onesWords !! h) (doubleToWords to) hundredsConnect
+
+connect :: String -> String -> (String, String) -> String
+connect "" second connect = second
+connect first "" connect = first ++ fst connect
+connect first second connect = first ++ fst connect ++ snd connect ++ second
 		
 makeTriple :: Int -> (Int, (Int, Int))
 makeTriple n = (quot n 100, (mod (quot n 10) 10, mod n 10))
@@ -29,8 +30,12 @@ makeTriple n = (quot n 100, (mod (quot n 10) 10, mod n 10))
 threeDigitsToWords :: Int -> String
 threeDigitsToWords = tripleToWords . makeTriple
 
+commas :: Int -> [Int]
+commas 0 = []
+commas n = mod n 1000 : (commas $ quot n 1000)
+
 sayIt :: Int -> String
-sayIt n = "number"
+sayIt n = "num-ber ,"
 
 countTo :: Int -> [String]
 countTo n = map sayIt [1..n]
