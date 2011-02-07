0537d7068322cae315038877e0e6df4b5f871951
Eric Wilson <ewilson@pillartechnology.com>
2011-02-07 13:03:18 -0500

    added to 14

--- 014.hs
+++ 014.hs
@@ -5,7 +5,23 @@ loop :: (Integral a) => a -> [a]
 loop 1 = [1]
 loop n = n : (loop $ next n)
 
+hasPreviousSmall :: (Integral a) => a -> Bool
+hasPreviousSmall n = mod n 6 == 4 && n /= 4
+ 
+previousSmall :: (Integral a) => a -> a
+previousSmall n = quot (n-1) 3
 
+allPrevious :: (Integral a) => a -> [a]
+allPrevious n = 
+	if (hasPreviousSmall n)
+		then [n * 2, previousSmall n]
+		else [n * 2]
+
+listPrevious :: (Integral a) => [a] -> [a]
+listPrevious [] = []
+listPrevious (x:xs) = allPrevious x ++ listPrevious xs
+
+		
 next :: (Integral a) => a -> a
 next n = 
 	if mod n 2 == 0
