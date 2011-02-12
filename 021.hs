f8b348825c376625e68a3627617108a40c5137e4
Eric Wilson <ewilson@pillartechnology.com>
2011-02-12 07:41:42 -0500

    refactored 21 for performance

--- 021.hs
+++ 021.hs
@@ -1,12 +1,9 @@
-import EulerMath.Divisors
-
--- I could optimize this, or I could move on to other problems . . .
-properDivs :: (Integral a) => a -> [a]
-properDivs n = [ d | d <- [1..n2], mod n d == 0 ]
-	where n2 = quot n 2
+properDivPairs :: (Integral a) => a -> [(a,a)]	
+properDivPairs n = takeWhile (\(a, b) -> a <= b) [ (d, quot n d) | d <- [1..quot n 2], mod n d == 0 ]
 
 d :: (Integral a) => a -> a
-d = sum . properDivs
+d n = divisorSum n - n
+	where divisorSum = sum . map (uncurry (+)) . properDivPairs
 
 hasAmicablePair :: (Integral a) => a -> Bool
 hasAmicablePair n = d dn == n && n /= dn
